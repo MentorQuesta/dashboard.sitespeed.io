e67de8b98a2f26c8d9ab64fcd79a305c73015552
Ramy Siha <ramysiha@gmail.com>
2023-07-11 12:03:52 +0200

    Added initial test for PUMA server

--- run.sh
+++ run.sh
@@ -5,31 +5,19 @@
 # DOCKER_CONTAINER=sitespeedio/sitespeed.io:16.2.0
 
 DOCKER_CONTAINER=sitespeedio/sitespeed.io-autobuild:main
-DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v /config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 "
+DOCKER_SETUP="--cap-add=NET_ADMIN  --shm-size=2g --rm -v "$(pwd)"/config:/config -v "$(pwd)":/sitespeed.io -v /etc/localtime:/etc/localtime:ro -e MAX_OLD_SPACE_SIZE=3072 --network=host  "
 DESKTOP_BROWSERS=(chrome firefox)
-EMULATED_MOBILE_BROWSERS=(chrome)
 
 # We loop through the desktop directory
 
-for file in tests/desktop/*.{txt,js} ; do
+for file in tests/desktop/*.js ; do
     for browser in "${DESKTOP_BROWSERS[@]}" ; do
         FILENAME=$(basename -- "$file")
         FILENAME_WITHOUT_EXTENSION="${FILENAME%.*}"
         CONFIG_FILE="config/$FILENAME_WITHOUT_EXTENSION.json"
         [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" || echo "Missing config file $CONFIG_FILE"
-        docker run $DOCKER_SETUP $DOCKER_CONTAINER --config $CONFIG_FILE -b $browser $file
-        control
-    done
-done
-
-for file in tests/emulatedMobile/*.{txt,js} ; do
-    for browser in "${EMULATED_MOBILE_BROWSERS[@]}" ; do
-        FILENAME=$(basename -- "$file")
-        FILENAME_WITHOUT_EXTENSION="${FILENAME%.*}"
-        CONFIG_FILE="config/$FILENAME_WITHOUT_EXTENSION.json"
-        [[ -f "$CONFIG_FILE" ]] && echo "Using config file $CONFIG_FILE" || echo "Missing config file $CONFIG_FILE"
-        docker run $DOCKER_SETUP $DOCKER_CONTAINER --config $CONFIG_FILE -b $browser $file
-        control
+        docker run $DOCKER_SETUP $DOCKER_CONTAINER --config $CONFIG_FILE -b $browser $file --multi
+        #control
     done
 done
 
@@ -37,5 +25,5 @@ done
 # If you run a stable version (as YOU should), you don't need to remove the container,
 # instead make sure you remove all volumes (of data)
 # docker volume prune -f
-docker system prune --all --volumes -f
+# docker system prune --all --volumes -f
 sleep 20
